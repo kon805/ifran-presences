4eb81441b6d7fd6b43db2754dd782ef95c1d3187
kon805 <kdaou048@gmail.com>
2025-07-06 07:53:18 +0000

    creation de  mes migrations et models (presence, cours)

---
+++ database/migrations/2025_07_06_074608_create_presences_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('presences', function (Blueprint $table) {
+            $table->id();
+             $table->foreignId('cours_id')->constrained()->onDelete('cascade');
+            $table->foreignId('etudiant_id')->constrained('users')->onDelete('cascade');
+            $table->enum('statut', ['présent', 'retard', 'absent']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('presences');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClasseController;
+use App\Http\Controllers\PresenceController;
+use App\Http\Controllers\CoursController;
 
 
 
@@ -30,10 +33,14 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/redirect-by-role', functi
         Route::get('/classes/{id}/edit', [\App\Http\Controllers\ClasseController::class, 'edit'])->name('coordinateur.classes.edit');
         Route::put('/classes/{id}', [\App\Http\Controllers\ClasseController::class, 'update'])->name('coordinateur.classes.update');
         Route::delete('/classes/{id}', [\App\Http\Controllers\ClasseController::class, 'destroy'])->name('coordinateur.classes.delete');
+         Route::resource('emploi-du-temps', App\Http\Controllers\CoursController::class);
     });
 
     Route::middleware('role:professeur')->prefix('professeur')->group(function () {
         Route::get('/', fn () => view('professeur.dashboard'))->name('professeur.dashboard');
+          Route::get('/mes-cours', [App\Http\Controllers\PresenceController::class, 'index'])->name('presences.index');
+          Route::get('/presences/{cours}', [App\Http\Controllers\PresenceController::class, 'edit'])->name('presences.edit');
+          Route::post('/presences/{cours}', [App\Http\Controllers\PresenceController::class, 'store'])->name('presences.store');
     });
 
     Route::middleware('role:etudiant')->prefix('etudiant')->group(function () {
